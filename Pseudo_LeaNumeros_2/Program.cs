5998c7d1021234166e037c94facd300bbded5a26
rasmorell-gif <61146061+rasmorell-gif@users.noreply.github.com>
2021-03-27 14:12:32 -0400

    Ejerccios

---
+++ Pseudo_LeaNumeros_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Pseudo_LeaNumeros_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite un número entero de tres digitos");
+            int num;
+            num = Convert.ToInt32(Console.ReadLine());
+
+          
+           if(num >99 && num<1000)
+           {    
+               num = num % 10;
+               int num1 = num % 10;
+               int num2 = num1 % 10;
+              
+               if (num!=num1 && num1 != num2 )
+               {
+                    Console.WriteLine("NO Existen  digitos iguales");
+               }
+               else if(num==num1 || num1 == num2 )
+               {
+                   Console.WriteLine("Existen 2 digitos iguales");
+               }
+               else if (num==num1 && num1 == num2)
+               {
+                   Console.WriteLine("Los 3 digitos son  iguales");
+               }
+
+                    
+            }else 
+            {
+               Console.WriteLine("Debe digitar un numero que tenga 3 digitos");
+           
+        }
+    }
+}
---
+++ Pseudo_LeaNumeros_3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Pseudo_LeaNumeros_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite un número entero ");
+            int num;
+            num = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+
+          for(int i = 0; i > num / 2 ;)
+          {
+             if(num / 2 != 0 ) 
+             {
+                 count++;
+             }
+          } Console.WriteLine($"Los pares contenidos en {num} son {count}");                
+            }else 
+            
+               Console.WriteLine("");
+             }
+           
+        }
+    }
+}
